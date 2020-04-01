502f4e33d6efe95970b27d72383d5613d3f35129
syu-h81 <58079996+syu-h81@users.noreply.github.com>
2020-04-01 12:34:36 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function() {
+
+  var fig = "close";
+  $('.btn').click(function() {
+    $('.profile-contents').toggle('slow');
+
+    if(fig == 'close') {
+      $(this).text('プロフィールを閉じる');
+      fig = 'open';
+    } else {
+      $(this).text('プロフィールを見る');
+      fig = 'close';
+    }
+  });
+
+  //$('#menu-show').click(
+    //function() {
+      //$('.menu-nav-wrapper').slideToggle();
+    //});
+
+});
